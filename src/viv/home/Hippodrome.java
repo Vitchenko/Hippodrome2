82fb05a88e4c7edc809c01edc79b63498017b801
Vitchenko <Euraeuspa12!>
2017-04-12 22:54:26 +0300

    com 12.04.2017

--- src/viv/home/Hippodrome.java
+++ src/viv/home/Hippodrome.java
@@ -1,20 +1,82 @@
 package viv.home;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by viv on 12.04.2017.
  */
 public class Hippodrome {
 
-    private ArrayList<Horse> horses= new ArrayList<>();
+    public static  ArrayList<Horse> horses = new ArrayList<>();
+
+    public static Hippodrome game;
 
     public ArrayList<Horse> getHorses() {
         return horses;
     }
 
-    public static void main(String[] args) {
+    public void run() throws InterruptedException {
+        for(int i=1; i<10;i++ ){
+         move();
+         print();
+         Thread.sleep(200);
+        }
+    }
 
+    public void move(){
+
+        for(int i=0;i<getHorses().size();i++){
+            getHorses().get(i).move();
+        }
 
     }
+
+
+    public void print(){
+
+        for(int i=0;i<getHorses().size();i++){
+            getHorses().get(i).print();
+        }
+
+        System.out.println();
+        System.out.println();
+
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        game = new Hippodrome();
+        Horse horse1 = new Horse("Hor1", 3, 0);
+        Horse horse2 = new Horse("Hor2", 3, 0);
+        Horse horse3 = new Horse("Hor3", 3, 0);
+
+        game.getHorses().add(horse1);
+        game.getHorses().add(horse2);
+        game.getHorses().add(horse3);
+
+        game.run();
+
+        game.printWinner();
+    }
+
+
+    public Horse getWinner(){
+
+        Collections.sort(horses);
+        return horses.get(horses.size()-1);
+    }
+
+    public void printWinner(){
+        System.out.println("Winner is " + getWinner().getName() + "!");
+
+        for(Horse arr:horses){
+            System.out.println(arr.getName() + " distance " + arr.getDistance());
+        }
+    }
+
+
+
 }
--- src/viv/home/Horse.java
+++ src/viv/home/Horse.java
@@ -1,7 +1,66 @@
 package viv.home;
 
+
+
 /**
  * Created by viv on 12.04.2017.
  */
-public class Horse {
+public class Horse implements Comparable<Horse>{
+
+    private String name;
+    private double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void move(){
+
+        distance += speed*Math.random()*10;
+
+    }
+    public void print(){
+
+        StringBuffer strDist=new StringBuffer();
+        int distance=(int)Math.round(this.distance);
+
+        for(int i=0;i<distance;i++){
+            strDist.append(".");
+        }
+        System.out.println(strDist+this.getName());
+
+
+    }
+
+    @Override
+    public int compareTo(Horse o) {
+        return (int)Math.round(this.getDistance()-o.getDistance());
+    }
 }
