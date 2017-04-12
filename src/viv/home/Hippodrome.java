fbf0394fdb87de3a1460711c4a74a5e9f9a5561d
Vitchenko <Euraeuspa12!>
2017-04-12 17:25:46 +0300

    Initial commit

---
+++ src/viv/home/Hippodrome.java
@@ -0,0 +1,20 @@
+package viv.home;
+
+import java.util.ArrayList;
+
+/**
+ * Created by viv on 12.04.2017.
+ */
+public class Hippodrome {
+
+    private ArrayList<Horse> horses= new ArrayList<>();
+
+    public ArrayList<Horse> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/viv/home/Horse.java
@@ -0,0 +1,7 @@
+package viv.home;
+
+/**
+ * Created by viv on 12.04.2017.
+ */
+public class Horse {
+}
